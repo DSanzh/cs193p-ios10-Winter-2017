25c1bb3e6c4f2d8fd14bd31c8d45846375f3d908
DSanzh <dauylov.sanzhar@gmail.com>
2017-10-27 14:59:43 +0600

    Finish lecture4

---
+++ FaceIt-Lecture4-Practice/FaceIt/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Sanzhar on 10/27/17.
+//  Copyright © 2017 Sanzhar Dauylov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var faceView: FaceView!
+  
+  
+  
+}
+
